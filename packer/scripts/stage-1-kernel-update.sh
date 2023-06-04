01ab4eefee67e67432a24d9c720e779aa6460285
AntonioZ31 <133108440+AntonioZ31@users.noreply.github.com>
2023-06-05 01:44:00 +0600

    Update stage-1-kernel-update.sh

--- packer/scripts/stage-1-kernel-update.sh
+++ packer/scripts/stage-1-kernel-update.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+cd /etc/yum.repos.d/
 
+# Переключение на репозиторий stream
+dnf --disablerepo '*' --enablerepo=extras swap centos-linux-repos centos-stream-repos
+dnf distro-sync
+
+sudo yum update
 # Установка репозитория elrepo
-sudo yum install -y https://www.elrepo.org/elrepo-release-8.el8.elrepo.noarch.rpm 
-# Установка нового ядра из репозитория elrepo-kernel
-yum --enablerepo elrepo-kernel install kernel-ml -y
+sudo rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+sudo yum install https://www.elrepo.org/elrepo-release-8.el8.elrepo.noarch.rpm -y
+# Установка нового ядра
+sudo yum --enablerepo elrepo-kernel install kernel-ml -y
 
 # Обновление параметров GRUB
 grub2-mkconfig -o /boot/grub2/grub.cfg
